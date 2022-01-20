6661f22ede7dd66ad52f71c7f02b5a2c0c779d60
Abdi <abditorres71@gmail.com>
2022-01-19 21:48:13 -0700

    jan 19

--- Jenkins the Monster Hunter/src/app.ts
+++ Jenkins the Monster Hunter/src/app.ts
@@ -1,40 +1,38 @@
 abstract class Person {
     abstract name: string;
+    
 
     display(): void{
         console.log(this.name);
+        
     }
+    
 }
 
 class Employee extends Person { 
-    name: string;
-    empCode: number;
     
-    constructor(name: string, code: number) { 
+    name: string;
+    latitude: number;
+    longitude: number;
+    weaknesses: number;
+    speed: number;
+    power: number;
+    
+    constructor(name: string, latitude: number, longitude: number, weaknesses: number, speed: number, power: number) { 
         super(); // must call super()
         
-        this.empCode = code;
         this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.weaknesses = weaknesses;
+        this.speed = speed;
+        this.power = power;
     }
 }
 
-let emp: Person = new Employee("James", 100);
+let emp: Person = new Employee("James", 90, 180, 35, 78, 60);
 emp.display(); //James
 
-class Circle {
-    static pi = 3.14;
-
-    static calculateArea(radius:number) {
-        return this.pi * radius * radius;
-    }
-
-    calculateCircumference(radius:number):number { 
-        return 2 * Circle.pi * radius;
-    }
-}
-
-Circle.calculateArea(5); // returns 78.5
-
-let circleObj = new Circle();
-circleObj.calculateCircumference(5) // returns 31.4000000
-//circleObj.calculateArea(); <-- cannot call this
+var names = ['Automatons', 'Bane', 'Bigfoot', 'Drakon', 'Minotaur', 'Wolfman'];
+names.sort();
+console.log(names);
